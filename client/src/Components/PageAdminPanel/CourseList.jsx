8d386202d2fcacbaf83925c269f0e4a5f9ef9dbe
Hiba <hbiba791@gmail.com>
2024-02-13 12:21:34 -0800

    commit

--- client/src/Components/PageAdminPanel/CourseList.jsx
+++ client/src/Components/PageAdminPanel/CourseList.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import DashboardNav from './DashboardNav'
+import { BiMenuAltLeft } from "react-icons/bi";
+import { CiSearch } from "react-icons/ci";
 
 const CourseList = () => {
   return (
-    <div className="ml-[300px]">
-      <h1 className="text-2xl font-bold mt-8">Courses</h1>
-      <p className="mt-4">This is the main content area of your Home.</p>
+    <div className="pl-[340px] mr-[100px] mt-5">
+      <DashboardNav/> 
+
+      <div>
+
+        <div className='flex justify-between items-center mt-10'>
+          <p className='text-bg-toggle text-4xl font-bold'>Manage Courses</p>
+          <button className='rounded-3xl bg-first-color text-white px-8 py-2'>Add Course</button>
+        </div>
+
+        <div className='mt-4'> 
+          <button className='bg-first-color rounded flex items-center gap-2 text-white px-8 py-2'><BiMenuAltLeft className='h-5 w-5'/> Filter</button>
+          <div className='flex items-center rounded-[5px]'>
+            
+            <input
+              type="text"
+              className='border border-light-blue/50 h-[50px] outline-none pl-10 w-[350px] rounded-[5px] placeholder:text-[14px] leading-[20px] font-normal '
+              placeholder='Search Class, Documents, Activities...'
+            />
+            <CiSearch className='text-bg-toggle w-6 h-6 absolute translate-x-3 cursor-pointer'/>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
