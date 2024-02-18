59a1400c513f6447128de3d0870c83fbfd7bb525
Hiba <hbiba791@gmail.com>
2024-02-18 14:15:27 -0800

    commit

--- client/src/Components/PageAdminPanel/Students.jsx
+++ client/src/Components/PageAdminPanel/Students.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import DashboardNav from './DashboardNav';
 
 const Students = () => {
   return (
-    <div className="ml-[300px]">
-      <h1 className="text-2xl font-bold mt-8">Students</h1>
-      <p className="mt-4">This is the main content area of your Students.</p>
+    <div className='pl-[340px] mr-[100px] mt-5'>
+            <DashboardNav/>
+            <div className='mt-10'>
+                <p className='text-bg-toggle text-4xl font-semibold'>Students</p>
+                <p className='mt-1'>View your Students here</p>
+            </div>
+
+            <div className='rounded-lg shadow-lg shadow-gray-400 mt-6'>
+                <p className='text-bg-toggle text-xl font-semibold py-4 px-6'>Students List</p>
+                <hr className='border-gray-400'/>
+
+            </div>
     </div>
   );
 };
