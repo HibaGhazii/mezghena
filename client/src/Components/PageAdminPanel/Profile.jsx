5bbf71bb554aca214a8a2068a7f6b4db911bf588
Hiba <hbiba791@gmail.com>
2024-02-20 13:00:00 -0800

    commit

--- client/src/Components/PageAdminPanel/Profile.jsx
+++ client/src/Components/PageAdminPanel/Profile.jsx
@@ -3,9 +3,26 @@ import DashboardNav from './DashboardNav'
 import image from "../assets/img/howitwork.png";
 import { TiLocation } from "react-icons/ti";
 import { TbWorld } from "react-icons/tb";
+import { BsCheck2 } from "react-icons/bs";
+import { BsX } from "react-icons/bs";
 
 const Profile = () => {
     const studentQueries = [
+        {
+            image:image,
+            title:'Machine learning Bootcamp',
+            teacher:'Brandon Taylor'
+        },
+        {
+            image:image,
+            title:'Machine learning Bootcamp',
+            teacher:'Brandon Taylor'
+        },
+        {
+            image:image,
+            title:'Machine learning Bootcamp',
+            teacher:'Brandon Taylor'
+        },
         {
             image:image,
             title:'Machine learning Bootcamp',
@@ -67,20 +84,30 @@ const Profile = () => {
             <p className='text-bg-toggle text-xl font-semibold py-4 px-6'>Student Queries</p>
             <hr className='border-2 border-blue-200'/>
 
-            {studentQueries.map((query, index) => (
-                <React.Fragment key={index}>
-                    <div className='flex gap-2 items-center my-2'>
-                        <img src={query.image} className='w-14 h-14 rounded-full mx-4'/>
-                        <div className='flex flex-col text-left'>
-                            <p className='text-bg-toggle font-bold text-lg'>{query.title}</p>
-                            <p className='text-gray-600 text-sm'>{query.teacher}</p>
+            <div className='overflow-y-scroll max-h-[210px]'>
+                {studentQueries.map((query, index) => (
+                    <React.Fragment key={index}>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex gap-2 items-center my-2'>
+                                <img src={query.image} className='w-10 h-10 rounded-full mx-4'/>
+                                <div className='flex flex-col text-left'>
+                                    <p className='text-bg-toggle font-bold text-lg'>{query.title}</p>
+                                    <p className='text-gray-600 text-sm'>{query.teacher}</p>
+                                </div>
+                            </div>
+                            <div className='flex items-center gap-3'>
+                                <button className='bg-green-300 rounded-full p-2'><BsCheck2 className='w-4 h-4'/></button> 
+                                <button className='bg-red-300 rounded-full p-2'><BsX className='w-4 h-4'/></button>
+                            </div>
                         </div>
-                    </div>
-                    {index !== studentQueries.length - 1 && <hr className='border-2 border-blue-200'/>}
-                </React.Fragment>
-            ))}
+                        {index !== studentQueries.length - 1 && <hr className='border-2 border-blue-200'/>}
+                        
+                    </React.Fragment> 
+                ))}
+            </div>
             
         </div>
+
     </div>
 
     </div>
